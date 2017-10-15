779f22af986f318af539fa8ca14b4f655354b59e
Amine M Laz <amine.benatia17@gmail.com>
2017-10-15 20:45:11 +0200

    Add Login & Logout routes

--- server/boot/00-routes.js
+++ server/boot/00-routes.js
@@ -5,44 +5,51 @@ module.exports = function (app) {
   User.create({email:'admin@coffeeshoprestaurant.org', password:'admin'}, function (err, cb) {
     console.log(cb);
   });
-  // Install a "/ping" route that returns "pong"
-  // app.get('/', function (req, res, next) {
-  //   if (req.isAuthenticated) {
-  //     res.render('/accueil', {
-  //       title: 'Accueil',
-  //       user: req.user
-  //     });
-  //   } else {
-  //     res.redirect('/login');
-  //   }
-  // });
-  //
-  //
-  // app.post('/login', function (req, res) {
-  //   User.login({
-  //     email: req.body.email,
-  //     password: req.body.password
-  //   }, 'user', function (err, token) {
-  //     if (err) {
-  //       if (err.details && err.code === 'LOGIN_FAILED_EMAIL_NOT_VERIFIED') {
-  //         res.render('reponseToTriggerEmail', {
-  //           title: 'Login failed',
-  //           content: err,
-  //           redirectToEmail: '/api/users/' + err.details.userId + '/verify',
-  //           redirectTo: '/',
-  //           redirectToLinkText: 'Click here',
-  //           userId: err.details.userId
-  //         });
-  //       } else {
-  //         res.render('response', {
-  //           title: 'Login failed. Wrong username or password',
-  //           content: err,
-  //           redirectTo: '/',
-  //           redirectToLinkText: 'Please login again',
-  //         });
-  //       }
-  //       return;
-  //     }
-  //   });
-  // });
+
+  app.get('/', function (req, res, next) {
+    if (req.isAuthenticated) {
+      res.render('/home', {
+        title: 'Accueil',
+        user: req.user
+      });
+    } else {
+      res.redirect('/login');
+    }
+  });
+
+  app.post('/login', function (req, res) {
+    User.login({
+      email: req.body.email,
+      password: req.body.password
+    }, 'user', function (err, token) {
+      if (err) {
+        if (err.details && err.code === 'LOGIN_FAILED_EMAIL_NOT_VERIFIED') {
+          res.render('reponseToTriggerEmail', {
+            title: 'Login failed',
+            content: err,
+            redirectToEmail: '/api/users/' + err.details.userId + '/verify',
+            redirectTo: '/',
+            redirectToLinkText: 'Click here',
+            userId: err.details.userId
+          });
+        } else {
+          res.render('response', {
+            title: 'Login failed. Wrong username or password',
+            content: err,
+            redirectTo: '/',
+            redirectToLinkText: 'Please login again',
+          });
+        }
+        return;
+      }
+    });
+  });
+
+  app.get('/logout', function(req, res, next) {
+    if (!req.accessToken) return res.sendStatus(401);
+    User.logout(req.accessToken.id, function(err) {
+      if (err) return next(err);
+      res.redirect('/');
+    });
+  });
 }
